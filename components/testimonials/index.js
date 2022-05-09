ddd481141523ae0bd507276554ee6ce8e3fb8145
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2022-05-09 13:27:55 +0700

    Add slider to testimonials

--- components/testimonials/index.js
+++ components/testimonials/index.js
@@ -5,15 +5,25 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
 
+import { useKeenSlider } from 'keen-slider/react';
 import exampleOne from '../../assets/img/example-1.png';
 import exampleTwo from '../../assets/img/example-2.png';
 import exampleThree from '../../assets/img/example-3.png';
 import exampleFour from '../../assets/img/example-4.png';
 
+import 'keen-slider/keen-slider.min.css';
+
 import makeStyles from './styles';
 
 const Testimonials = () => {
     const styles = makeStyles();
+
+    const [refCallback] = useKeenSlider({
+        loop: true,
+        slides: {
+            perView: 2,
+        },
+    });
 
     return (
         <Container maxWidth="xl" sx={styles.testimonialWrapper}>
@@ -21,36 +31,62 @@ const Testimonials = () => {
                 <Typography sx={styles.title}>What They Said ?</Typography>
             </Box>
             <Box sx={styles.contentWrapper}>
-                <Grid container spacing={2} sx={styles.gridRow}>
-                    <Grid item xs={6}>
+                <div ref={refCallback} className="keen-slider">
+                    <div className="keen-slider__slide">
                         <Box sx={styles.testimonialItem}>
-                            <Typography sx={styles.bigComma}>
-                                &ldquo;
-                            </Typography>
+                            <Typography sx={styles.bigComma}>&ldquo;</Typography>
                             <Typography sx={styles.testimonialItem__text}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                             </Typography>
-                            <Typography sx={styles.testimonialItem__people}>
-                                - Customer Name -
-                            </Typography>
+                            <Typography sx={styles.testimonialItem__people}>- Customer Name -</Typography>
                         </Box>
-                    </Grid>
-                    <Grid item xs={6}>
+                    </div>
+                    <div className="keen-slider__slide">
                         <Box sx={styles.testimonialItem}>
-                            <Typography sx={styles.bigComma}>
-                                &ldquo;
-                            </Typography>
+                            <Typography sx={styles.bigComma}>&ldquo;</Typography>
                             <Typography sx={styles.testimonialItem__text}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                             </Typography>
-                            <Typography sx={styles.testimonialItem__people}>
-                                - Customer Name -
-                            </Typography>
+                            <Typography sx={styles.testimonialItem__people}>- Customer Name -</Typography>
                         </Box>
-                    </Grid>
-                </Grid>
+                    </div>
+                    <div className="keen-slider__slide">
+                        <Box sx={styles.testimonialItem}>
+                            <Typography sx={styles.bigComma}>&ldquo;</Typography>
+                            <Typography sx={styles.testimonialItem__text}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </Typography>
+                            <Typography sx={styles.testimonialItem__people}>- Customer Name -</Typography>
+                        </Box>
+                    </div>
+                    <div className="keen-slider__slide">
+                        <Box sx={styles.testimonialItem}>
+                            <Typography sx={styles.bigComma}>&ldquo;</Typography>
+                            <Typography sx={styles.testimonialItem__text}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </Typography>
+                            <Typography sx={styles.testimonialItem__people}>- Customer Name -</Typography>
+                        </Box>
+                    </div>
+                    <div className="keen-slider__slide">
+                        <Box sx={styles.testimonialItem}>
+                            <Typography sx={styles.bigComma}>&ldquo;</Typography>
+                            <Typography sx={styles.testimonialItem__text}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </Typography>
+                            <Typography sx={styles.testimonialItem__people}>- Customer Name -</Typography>
+                        </Box>
+                    </div>
+                    <div className="keen-slider__slide">
+                        <Box sx={styles.testimonialItem}>
+                            <Typography sx={styles.bigComma}>&ldquo;</Typography>
+                            <Typography sx={styles.testimonialItem__text}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </Typography>
+                            <Typography sx={styles.testimonialItem__people}>- Customer Name -</Typography>
+                        </Box>
+                    </div>
+                </div>
             </Box>
             <Box sx={styles.exampleWrapper}>
                 <Grid container maxWidth="xl" spacing={1}>
