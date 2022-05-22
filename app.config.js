9662ac81a1605df2a238a9a53dec5b6f8b2f5aa5
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2022-05-22 20:34:18 +0700

    Deployment

--- app.config.js
+++ app.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     db: {
-        host: 'localhost',
+        host: '103.253.212.95',
         user: 'root',
         password: '',
         database: 'mantenstory',
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://mantenstory.co',
     encryption: {
         key: 'TXAjwm8k53PJG9NacLbyZavvQB2qBh43',
         algorithm: 'aes-256-cbc',
